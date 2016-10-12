ee4182b821ff461eacb52c12232a41646d5f1d93
Ernie Munoz <emunoz@fairwaytech.com>
2016-10-12 16:18:43 -0700

    Adding Dockerfile/startup script

---
+++ service/src/main/docker/docker-startup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#-----------------------------------------------------------------------------------------------------------------------
+# File:  docker-startup.sh
+#
+# Desc:  Start the tds-assessment-service.jar with the appropriate properties.
+#
+#-----------------------------------------------------------------------------------------------------------------------
+
+# Determine the amount of memory available to the container, for use with setting the Xms and Xmx values when starting
+# the jar (in this case setting both values to 80% of free memory).
+freeMem=`awk '/MemFree/ { print int($2/1024) }' /proc/meminfo`
+s=$(($freeMem/10*8))
+x=$(($freeMem/10*8))
+
+java \
+    -Dspring.datasource.url="jdbc:mysql://${ASSESSMENT_DB_HOST}:${ASSESSMENT_DB_PORT}/${ASSESSMENT_DB_NAME}" \
+    -Dspring.datasource.username="${ASSESSMENT_DB_USER}" \
+    -Dspring.datasource.password="${ASSESSMENT_DB_PASSWORD}" \
+    -Dspring.datasource.type=com.zaxxer.hikari.HikariDataSource \
+    -jar /tds-assessment-service.jar \
+    --server-port="8080" \
+    --server.undertow.buffer-size=16384 \
+    --server.undertow.buffers-per-region=20 \
+    --server.undertow.io-threads=64 \
+    --server.undertow.worker-threads=512 \
+    --server.undertow.direct-buffers=true \
