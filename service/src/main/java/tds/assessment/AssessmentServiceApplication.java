5e79fdaf8ce1ff033c3552003b3c594ff47f09b2
Ernie Munoz <emunoz@fairwaytech.com>
2016-11-18 12:21:06 -0800

    Moving DataSoureConfiguration to TDS_Common

--- service/src/main/java/tds/assessment/AssessmentServiceApplication.java
+++ service/src/main/java/tds/assessment/AssessmentServiceApplication.java
@@ -1,13 +1,13 @@
 package tds.assessment;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+
+import tds.common.configuration.DataSourceConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@Import(DataSourceConfiguration.class)
 public class AssessmentServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AssessmentServiceApplication.class, args);
