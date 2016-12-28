ba5b250ef4c962f8582215b2dc298508491bb7bd
Todd Miller <tmiller@fairwaytech.com>
2016-12-27 19:47:39 -0800

    Add jackson configuration to assessment.  Moved to the correct location.

--- service/src/main/java/tds/assessment/configuration/AssessmentServiceApplicationConfiguration.java
+++ service/src/main/java/tds/assessment/configuration/AssessmentServiceApplicationConfiguration.java
@@ -1,12 +1,17 @@
-package tds.assessment.repositories;
+package tds.assessment.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import tds.common.configuration.DataSourceConfiguration;
+import tds.common.configuration.JacksonObjectMapperConfiguration;
 import tds.common.web.advice.ExceptionAdvice;
 
 @Configuration
-@Import({ExceptionAdvice.class, DataSourceConfiguration.class})
+@Import({
+    ExceptionAdvice.class,
+    DataSourceConfiguration.class,
+    JacksonObjectMapperConfiguration.class
+})
 public class AssessmentServiceApplicationConfiguration {
 }
