4eff09b6eebe1a4327e458fdd9b6846261ae5a11
Will <wtritch@users.noreply.github.com>
2017-02-16 14:18:34 -0800

    Externalize configuration properties for the assessment service. (#30)

--- service/src/main/docker/docker-startup.sh
+++ service/src/main/docker/docker-startup.sh
@@ -6,20 +6,4 @@
 #
 #-----------------------------------------------------------------------------------------------------------------------
 
-java \
-    -Dspring.datasource.url="jdbc:mysql://${ASSESSMENT_DB_HOST}:${ASSESSMENT_DB_PORT}/${ASSESSMENT_DB_NAME}" \
-    -Dspring.datasource.username="${ASSESSMENT_DB_USER}" \
-    -Dspring.datasource.password="${ASSESSMENT_DB_PASSWORD}" \
-    -Dspring.datasource.type=com.zaxxer.hikari.HikariDataSource \
-    -Dspring.datasource.driver-class-name=com.mysql.jdbc.Driver \
-    -Dspring.datasource.testWhileIdle=true \
-    -Dspring.datasource.timeBetweenEvictionRunsMillis=60000 \
-    -Dspring.datasource.validationQuery="SELECT 1" \
-    -Dflyway.enabled=false \
-    -jar /tds-assessment-service.jar \
-    --server-port="8080" \
-    --server.undertow.buffer-size=16384 \
-    --server.undertow.buffers-per-region=20 \
-    --server.undertow.io-threads=64 \
-    --server.undertow.worker-threads=512 \
-    --server.undertow.direct-buffers=true \
+java -jar /tds-assessment-service.jar
